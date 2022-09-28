e81af607b881989cb7cf49a3fe361845fd64d70a
Suraj Subramanian <suraj.tf6@gmail.com>
2022-09-28 19:16:41 +0530

    Update debian-docker-install.sh

--- debian-docker-install.sh
+++ debian-docker-install.sh
@@ -2,19 +2,20 @@
 
 # The script needs to run as root user
 
+# Install prerequisites 
 apt update
-apt install -y\
-	ca-certificates \
-	curl \
-	gnupg \
-	lsb-release
+apt install ca-certificates curl gnupg lsb-release
 
+# Add GPG key
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc//
+apt/keyrings/docker.gpg
 
-echo \ 
-  	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-	$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# setup repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker..
+gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo  
+tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# Install docker
 apt update
-apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin

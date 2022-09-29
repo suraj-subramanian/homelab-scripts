3dad61302056bdfbd3d41fb41dd541df90c76c00
Suraj Subramanian <suraj.tf6@gmail.com>
2022-09-29 22:08:50 +0530

    Update debian-docker-install.sh

--- debian-docker-install.sh
+++ debian-docker-install.sh
@@ -11,9 +11,9 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 # setup repository
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker..
-gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo  
-tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install docker
 apt update

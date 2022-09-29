972f9bdedc010ac306b7c4effd7f246aa5dc6354
Suraj Subramanian <suraj.tf6@gmail.com>
2022-09-29 22:05:03 +0530

    Update debian-docker-install.sh

--- debian-docker-install.sh
+++ debian-docker-install.sh
@@ -8,8 +8,7 @@ apt install ca-certificates curl gnupg lsb-release
 
 # Add GPG key
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc//
-apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 # setup repository
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker..
